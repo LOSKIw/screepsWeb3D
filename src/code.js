4b321d75bbbd67a500d7d028ab4c34957f83d5e5
LOSKIw <48372803+LOSKIw@users.noreply.github.com>
2021-04-28 22:13:09 +0800

    Update code.js

--- src/code.js
+++ src/code.js
@@ -14,6 +14,6 @@ function getData(){
 function getUrl(){
 	let room = document.getElementById('room').value;
 	let shard = document.getElementById('shard').value;
-	let url = 'https://cors-anywhere.herokuapp.com/https://screeps.com/api/game/room-terrain?shard=' + shard + '&room=' + room;
+	let url = 'https://screeps-cros.herokuapp.com/https://screeps.com/api/game/room-terrain?shard=' + shard + '&room=' + room;
 	return url
 }
