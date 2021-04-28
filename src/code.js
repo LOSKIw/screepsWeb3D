c22032f73649e531783bcc3a2ee338828e0b8dab
LOSKIw <48372803+LOSKIw@users.noreply.github.com>
2021-04-28 12:58:13 +0800

    attempt

--- src/code.js
+++ src/code.js
@@ -1,9 +1,14 @@
 function getData(){
+    let token = document.getElementById('token')
     axios.get('https://screeps-cros.herokuapp.com/https://screeps.com/api/game/room-terrain', {
         params: {
             'shard': "shard2",
             'room': "W4N49"
+        },
+        headers: {
+            'X-token': token
         }
+
       })
       .then(function (response) {
         console.log(response);
